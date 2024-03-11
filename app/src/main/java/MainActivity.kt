8b446f245ca66cc5b6af696d6dcfa116f78bb9a4
IPZ_CR_3 <tliskovchuk@gmail.com>
2024-03-11 14:54:44 +0200

    Додав номер телеону, пошту та тег

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,15 +1,16 @@
-package ua.edu.lntu.ipz_41_cr01_liskovchuk_serhii
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.ipz_41_cr01_liskovchuk_serhii.ui.theme.IPZ_41_CR01_Liskovchuk_SerhiiTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("jen.doe@android.com", "@AndroidDev", "+11 (123) 444 555 666")
                 }
             }
         }
@@ -30,17 +31,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(email: String, additionalText1: String, additionalText2: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = additionalText1,
+            color = Color.Black,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = additionalText2,
+            color = Color.Black,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+        Text(
+            text = email,
+            color = Color.Black,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IPZ_41_CR01_Liskovchuk_SerhiiTheme {
-        Greeting("Android")
+        Greeting("jen.doe@android.com", "@AndroidDev", "+11 (123) 444 555 666")
     }
 }
